37cd6d0aac7ec763746e5d76c18bb0313be6d36a
AndersJakobsen <ande12n3@easv365.dk>
2022-04-29 12:52:52 +0200

    CRUD for the CategoryDAO

--- src/DAL/CategoryDAO.java
+++ src/DAL/CategoryDAO.java
@@ -1,5 +1,102 @@
 package DAL;
 
+import BE.Category;
+
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
 public class CategoryDAO
 {
+    private DatabaseConnector connection;
+    public Category category;
+
+
+
+    public List<Category> getAllCategories(int citizenID) throws SQLException {
+        List<Category> categories = new ArrayList<>();
+
+        try(Connection conn = connection.getConnection()){
+            String sql = "SELECT * FROM Category WHERE CitizenID=?;";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, citizenID);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()){
+                int id = rs.getInt("ID");
+                String catName = rs.getString("CatName");
+
+                Category category = new Category(catName);
+                categories.add(category);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return categories;
+    }
+
+    /**
+     * Creates a Category
+     *
+     * @param CatName The name of Category
+     * @param CitizenID The ID of the Citizen
+     * @return the coordinator object
+     */
+    public void createCategory(String CatName, String CitizenID) {
+
+        try (Connection conn = connection.getConnection()) {
+            String sqlStatement = "INSERT INTO Category(CatName,CitizenID) VALUES (?, ?);";
+
+            try(PreparedStatement preparedStatement = conn.prepareStatement(sqlStatement, Statement.RETURN_GENERATED_KEYS)){
+                preparedStatement.setString(1, CatName);
+                preparedStatement.setString(2, CitizenID);
+                preparedStatement.execute();
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateCategory(Category category) throws SQLException {
+
+        try (Connection conn = connection.getConnection()) {
+            String sqlStatement = "UPDATE Category SET CatName=?, CitizenID=?";
+
+            try(PreparedStatement preparedStatement = conn.prepareStatement(sqlStatement, Statement.RETURN_GENERATED_KEYS)){
+                preparedStatement.setString(1, category.getCatName());
+                preparedStatement.setString(2, category.getCitizenID());
+                preparedStatement.setInt(3, category.getID());
+                if(preparedStatement.executeUpdate() != 1){
+                    throw new SQLException("Could not update Category");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void deleteCategory(int categoryID) throws SQLException {
+        try(Connection conn = connection.getConnection()){
+            String sql1 = "DELETE FROM SubCategory WHERE CategoryID=?";
+            String sql2 = "DELETE FROM Category WHERE ID=?";
+            PreparedStatement preparedStatement1 = conn.prepareStatement(sql1);
+            preparedStatement1.setInt(1,categoryID);
+            preparedStatement1.execute();
+
+            PreparedStatement preparedStatement2 = conn.prepareStatement(sql2);
+            preparedStatement2.setInt(1,categoryID);
+            preparedStatement2.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
