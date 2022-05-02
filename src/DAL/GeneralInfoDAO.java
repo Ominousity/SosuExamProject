003aef80e950905541e17eba1102df3c04d4af1b
Zeroject <cypeyt@gmail.com>
2022-05-02 11:21:58 +0200

    Update GeneralInfoDAO.java

--- src/DAL/GeneralInfoDAO.java
+++ src/DAL/GeneralInfoDAO.java
@@ -2,18 +2,19 @@ package DAL;
 
 import BE.GeneralInfo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.io.IOException;
+import java.sql.*;
 
 public class GeneralInfoDAO
 {
     private DatabaseConnector connection;
-    public GeneralInfoDAO(){
+    public GeneralInfoDAO() throws IOException
+    {
         connection = new DatabaseConnector();
     }
 
     public GeneralInfo getGeneralInfo(int CitizenID){
+        GeneralInfo generalInfo = new GeneralInfo("","","","","","","","","","","");
         try(Connection conn = connection.getConnection())
         {
             String sql = "SELECT * FROM GeneralInfo WHERE CitizenID=?;";
@@ -23,9 +24,33 @@ public class GeneralInfoDAO
             while (rs.next()){
                 String mestring = rs.getString("Mestring");
                 String motivation = rs.getString("Motivation");
-                String Ressourcer = rs.getString("Ressourcer");
+                String ressourcer = rs.getString("Ressourcer");
+                String roller = rs.getString("Roller");
+                String vaner = rs.getString("Vaner");
+                String uddanelseJob = rs.getString("UddanelseJob");
+                String livshistorie = rs.getString("Livshistorie");
+                String netværk = rs.getString("Netværk");
+                String helbredsoplysninger = rs.getString("Helbredsoplysninger");
+                String hjælpemidler = rs.getString("Hjølpemidler");
+                String boligensIndretning = rs.getString("BoligensIndretning");
+
+                generalInfo = new GeneralInfo(mestring, motivation, ressourcer, roller, vaner, uddanelseJob, livshistorie, netværk, helbredsoplysninger, hjælpemidler, boligensIndretning);
             }
-            GeneralInfo generalInfo = new GeneralInfo();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return generalInfo;
+    }
+
+    public void createGeneralInfo(String mestring, String motivation, String ressourcer, String roller, String vaner, String uddanelseJob, String livhistorie, String netværk, String helbresoplysninger, String hjælpemidler, String boligensIndretning){
+        try(Connection conn = connection.getConnection())
+        {
+            String sql = "INSERT INTO GeneralInfo(Mestring, Motivation, Ressourcer, Roller, Vaner, UddanelseJob, Livshistorie, Netværk, Helbredsoplysninger, Hjælpemidler, BoligensIndretning)" +
+                    "values(?,?,?,?,?,?,?,?,?,?,?);";
+            try(PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+            {
+                
+            }
         }
     }
 }
