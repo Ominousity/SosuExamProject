c6c8193d0291eed1d20a2c752daebf0c8d529903
Ominousity <abs@hotmail.dk>
2022-05-02 10:37:12 +0200

    updated citizen manager

--- src/BLL/CitizenManager.java
+++ src/BLL/CitizenManager.java
@@ -1,5 +1,23 @@
 package BLL;
 
+import DAL.CitizenDAO;
+
 public class CitizenManager {
-    
+    CitizenDAO citizenDAO;
+
+    public CitizenManager(){
+        citizenDAO = new CitizenDAO();
+    }
+
+    public void createCitizen(){
+        citizenDAO.createCitizen();
+    }
+
+    public void updateCitizen(){
+        citizenDAO.updateCitizen();
+    }
+
+    public void deleteCitizen(){
+        citizenDAO.deleteCitizen();
+    }
 }
