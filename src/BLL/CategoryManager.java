d4390a8391ab3ad6d8fb38ee8ff74f31342d584f
Ominousity <abs@hotmail.dk>
2022-05-02 11:02:43 +0200

    updates to managers

--- src/BLL/CategoryManager.java
+++ src/BLL/CategoryManager.java
@@ -5,8 +5,8 @@ import DAL.CategoryDAO;
 import DAL.SubCategoryDAO;
 
 public class CategoryManager {
-    CategoryDAO categoryDAO;
-    SubCategoryDAO subCategoryDAO;
+    private CategoryDAO categoryDAO;
+    private SubCategoryDAO subCategoryDAO;
 
     public CategoryManager(){
         categoryDAO = new CategoryDAO();
