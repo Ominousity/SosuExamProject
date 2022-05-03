42702d334acbdbf733f4a2b4004eb209df04ea79
AndersJakobsen <ande12n3@easv365.dk>
2022-05-03 09:41:55 +0200

    corrections

--- src/BLL/CategoryManager.java
+++ src/BLL/CategoryManager.java
@@ -4,6 +4,8 @@ import BE.SubCategory;
 import DAL.CategoryDAO;
 import DAL.SubCategoryDAO;
 
+import java.sql.SQLException;
+
 public class CategoryManager {
     private CategoryDAO categoryDAO;
     private SubCategoryDAO subCategoryDAO;
@@ -13,7 +15,7 @@ public class CategoryManager {
         subCategoryDAO = new SubCategoryDAO();
     }
 
-    public void updateSubCategory(String subCatContents){
+    public void updateSubCategory(SubCategory subCatContents) throws SQLException {
         subCategoryDAO.updateSubCategory(subCatContents);
     }
 }
