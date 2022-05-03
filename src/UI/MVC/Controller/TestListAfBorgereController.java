f94c2f6ce0f3958f35f9378d98258a43bc5f0cb8
AndersJakobsen <ande12n3@easv365.dk>
2022-05-03 14:08:21 +0200

    Oprettelse af test klasse til liste af borgere til specifikke students

---
+++ src/UI/MVC/Controller/TestListAfBorgereController.java
@@ -0,0 +1,14 @@
+package UI.MVC.Controller;
+
+import BE.Citizen;
+import BE.Student;
+import org.junit.jupiter.api.Test;
+
+public class TestListAfBorgereController {
+
+@Test
+    void
+
+
+    }
+}
