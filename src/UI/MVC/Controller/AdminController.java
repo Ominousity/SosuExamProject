7970fd1874e77ff4f08ed802632a5f3a551704f0
Math376i <91528384+Math376i@users.noreply.github.com>
2022-05-04 10:57:37 +0200

    LogOutBtn in Admin and Dashboard

--- src/UI/MVC/Controller/AdminController.java
+++ src/UI/MVC/Controller/AdminController.java
@@ -1,4 +1,17 @@
 package UI.MVC.Controller;
 
+import UI.Utility.SceneCreator;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class AdminController {
+SceneCreator sceneCreator = new SceneCreator();
+
+    public void OnExitBtn(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        sceneCreator.createScene("Login.fxml","",false,this);
+        stage.close();
+    }
 }
--- src/UI/MVC/Controller/DashboardController.java
+++ src/UI/MVC/Controller/DashboardController.java
@@ -1,4 +1,15 @@
 package UI.MVC.Controller;
 
+import UI.Utility.SceneCreator;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class DashboardController {
+    SceneCreator sceneCreator = new SceneCreator();
+    public void OnExitBtn(ActionEvent actionEvent) {
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            sceneCreator.createScene("Login.fxml","",false,this);
+            stage.close();
+    }
 }
