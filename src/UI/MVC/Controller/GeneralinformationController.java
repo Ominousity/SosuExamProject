ea12174f03e46d113da20d641cbd5a650eb35380
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2022-05-04 15:41:41 +0200

    general info and health ui

--- src/UI/MVC/Controller/GeneralinformationController.java
+++ src/UI/MVC/Controller/GeneralinformationController.java
@@ -1,4 +1,12 @@
 package UI.MVC.Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+
 public class GeneralinformationController {
+    public Pane pBackmenu;
+    public Pane pInfoMenu;
+    public TextArea taInfo;
+    public Button btnGinfoSave;
 }
--- src/UI/MVC/Controller/HealthController.java
+++ src/UI/MVC/Controller/HealthController.java
@@ -1,5 +1,13 @@
 package UI.MVC.Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+
 public class HealthController
 {
+    public Pane pBack2;
+    public Pane pMenu2;
+    public Button btnSaveHealth;
+    public TextArea taHealthInfo;
 }
