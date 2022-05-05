ea4da8e0d9236355a4c2eeda3922174153af2294
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2022-05-05 10:52:42 +0200

    fixed some UI issues

--- src/UI/MVC/Controller/FunktionsevneController.java
+++ src/UI/MVC/Controller/FunktionsevneController.java
@@ -1,4 +1,10 @@
 package UI.MVC.Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
 public class FunktionsevneController {
+    public Pane pBackFunction;
+    public Pane pMenuFunction;
+    public Button btnSaveFunction;
 }
