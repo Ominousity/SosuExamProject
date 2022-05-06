4b0abdbc7c5fb106c9267d71915fd65516b4c4f9
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2022-05-06 10:18:52 +0200

    added fxml tag to healthcontroller

--- src/UI/MVC/Controller/HealthController.java
+++ src/UI/MVC/Controller/HealthController.java
@@ -1,13 +1,18 @@
 package UI.MVC.Controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
 
 public class HealthController
 {
+    @FXML
     private Pane pBack2;
+    @FXML
     private Pane pMenu2;
+    @FXML
     private Button btnSaveHealth;
+    @FXML
     private TextArea taHealthInfo;
 }
