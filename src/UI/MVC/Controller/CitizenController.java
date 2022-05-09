1e34c60f4889dbc50833dd3a8a7c6b8e37fa5265
Zeroject <cypeyt@gmail.com>
2022-05-09 14:16:35 +0200

    Update CitizenController.java

--- src/UI/MVC/Controller/CitizenController.java
+++ src/UI/MVC/Controller/CitizenController.java
@@ -8,22 +8,43 @@ import UI.Utility.SceneCreator;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 
+import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class CitizenController {
+public class CitizenController implements Initializable
+{
+    private TextArea taCasetext;
+    private TableColumn tvCases;
+    private Label fNameLbl;
+    private Label lNameLbl;
+    private Label dobLbl;
+    private Label socialSecLbl;
+    private ImageView citizenImg;
+    private Button genInfoBtn;
+
     private SceneCreator sceneCreator;
+    private CitizenModel citizenModel;
 
-    public CitizenController() {
+    public CitizenController() throws IOException
+    {
         sceneCreator = new SceneCreator();
+        citizenModel = new CitizenModel();
     }
 
     public void handleBack(ActionEvent actionEvent) {
         sceneCreator.createStage(sceneCreator.createScene("../View/CitizenView.fxml","UI/CSS/MainStylesheet.css",this), "Borger", false);
     }
 
+    @Override public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
 }
