cbb63fdb3417c23dade39c6bba85a53e345ad9c3
Zeroject <cypeyt@gmail.com>
2022-05-09 12:33:27 +0200

    Update CreateCitizenController.java

--- src/UI/MVC/Controller/CreateCitizenController.java
+++ src/UI/MVC/Controller/CreateCitizenController.java
@@ -1,8 +1,13 @@
 package UI.MVC.Controller;
 
+import BE.Category;
+import BE.Citizen;
 import BE.Student;
+import BE.SubCategory;
+import UI.MVC.Model.CategoryModel;
 import UI.MVC.Model.CitizenModel;
 import UI.MVC.Model.StudentModel;
+import UI.MVC.Model.SubCategoryModel;
 import UI.Utility.SceneCreator;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -10,19 +15,27 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class CreateCitizenController implements Initializable
 {
-
+    @FXML
+    private TableView<Citizen> citizenTB;
+    @FXML
+    private TableColumn<Citizen, String> fNameTC;
+    @FXML
+    private TableColumn<Citizen, String> lNameTC;
     @FXML
     private ImageView citizenImg;
     @FXML
@@ -47,16 +60,19 @@ public class CreateCitizenController implements Initializable
     private SceneCreator sceneCreator;
     private CitizenModel citizenModel;
     private StudentModel studentModel;
+    private CategoryModel categoryModel;
+    private SubCategoryModel subCategoryModel;
 
     public CreateCitizenController() throws IOException
     {
         sceneCreator = new SceneCreator();
         citizenModel = new CitizenModel();
         studentModel = new StudentModel();
-    }
-
-    @Override public void initialize(URL location, ResourceBundle resources)
-    {
+        categoryModel = new CategoryModel();
+        subCategoryModel = new SubCategoryModel();
+        citizenTB = new TableView();
+        fNameTC = new TableColumn();
+        lNameTC = new TableColumn();
         fNameTextField = new TextField();
         lNameTextField = new TextField();
         dobDatePicker = new DatePicker();
@@ -65,6 +81,14 @@ public class CreateCitizenController implements Initializable
         isTemplate = new CheckBox();
         studentTextArea = new TextArea();
         chooseStudentCB = new ChoiceBox();
+    }
+
+    @Override public void initialize(URL location, ResourceBundle resources)
+    {
+        citizenTB.setPlaceholder(new Label("Ingen borgere var fundet i databasen"));
+
+        fNameTC.setCellValueFactory(new PropertyValueFactory<>("FName"));
+        lNameTC.setCellValueFactory(new PropertyValueFactory<>("LName"));
         try
         {
             ObservableList<Student> students = FXCollections.observableArrayList(studentModel.getAllStudents());
@@ -88,11 +112,29 @@ public class CreateCitizenController implements Initializable
     }
 
     public void handleCancel(ActionEvent actionEvent) {
-        sceneCreator.createStage(sceneCreator.createScene("../View/CitizenView.fxml","UI/CSS/MainStylesheet.css",this), "Borger", false);
+        Stage stage = (Stage) citizenImg.getScene().getWindow();
+        stage.close();
     }
 
     public void handleCreate(ActionEvent actionEvent) {
         citizenModel.createCitizen(fNameTextField.getText(), lNameTextField.getText(), String.valueOf(dobDatePicker.getValue()), adressTextField.getText(), socialSecTextField.getText());
+        ArrayList<String> categoryName = new ArrayList<>();
+        categoryName.add("Funktionsniveau");
+        categoryName.add("Bevægeapparat");
+        categoryName.add("Ernæring");
+        categoryName.add("Hud og slimhinder");
+        categoryName.add("Kommunikation");
+        categoryName.add("Psykosociale forhold");
+        categoryName.add("Respiration og cirkulation");
+        categoryName.add("Seksualitet");
+        categoryName.add("Smerter og sanseindtryk");
+        categoryName.add("Søvn og hvile");
+        categoryName.add("Viden og udvikling");
+        categoryName.add("Udskillelse af affaldsstoffer");
+        for (String cat : categoryName)
+        {
+            categoryModel.createCategory(cat, );
+        }
     }
 
     public void handleCancelTemplate(ActionEvent actionEvent) {
