96ed65163dcb796d2caa0de4f7dcf02b216cb8e1
Ominousity <abs@hotmail.dk>
2022-05-12 10:17:53 +0200

    css update

--- src/DAL/AdminDAO.java
+++ src/DAL/AdminDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class AdminDAO
 {
     private DatabaseConnector connection;
+
     public AdminDAO() throws IOException
     {
         connection = new DatabaseConnector();
