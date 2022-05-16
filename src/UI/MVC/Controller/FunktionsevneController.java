73f43c4f13eb992141021bf578a3e040d462b741
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2022-05-16 13:33:12 +0200

    create textfield per sub test

--- src/UI/MVC/Controller/FunktionsevneController.java
+++ src/UI/MVC/Controller/FunktionsevneController.java
@@ -2,8 +2,12 @@ package UI.MVC.Controller;
 
 import UI.Utility.SceneCreator;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 
-public class FunktionsevneController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FunktionsevneController implements Initializable {
 
     private SceneCreator sceneCreator;
 
@@ -14,5 +18,10 @@ public class FunktionsevneController {
         sceneCreator.createStage(sceneCreator.createScene("../View/CitizenView.fxml", "UI/CSS/MainStylesheet.css",this), "Borger", false);
     }
     public void handleSave(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
