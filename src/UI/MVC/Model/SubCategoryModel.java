cf91dde9b333dbd4ce75bebf050bbacb30ebdc78
Ominousity <abs@hotmail.dk>
2022-05-20 14:00:51 +0200

    fix

--- src/UI/MVC/Model/SubCategoryModel.java
+++ src/UI/MVC/Model/SubCategoryModel.java
@@ -2,6 +2,8 @@ package UI.MVC.Model;
 
 import BE.SubCategory;
 import BLL.SubCategoryManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -9,14 +11,22 @@ import java.util.List;
 
 public class SubCategoryModel {
 
+    private ObservableList subCatList;
+
     SubCategoryManager subCategoryManager;
-    public SubCategoryModel() throws IOException
-    {
+    public SubCategoryModel() throws IOException {
         subCategoryManager = new SubCategoryManager();
+        subCatList = FXCollections.observableArrayList();
     }
 
     public List<SubCategory> getSubCategories(int SubCategoryID) throws SQLException {
         return subCategoryManager.getSubCategories(SubCategoryID);
+    }
+
+    public List<SubCategory> getObservableSubCategories(int categoryID) throws SQLException {
+        subCatList.clear();
+        subCatList.addAll(subCategoryManager.getSubCategories(categoryID));
+        return subCatList;
     }
 
     public void createSubCategory(String subCatName, String subCatContents, int categoryID){
